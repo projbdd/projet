03956c6c274d39db1c546c852ccf59bb1a5f9bd3
AntoineGlb <78866642+AntoineGlb@users.noreply.github.com>
2021-02-14 00:38:13 +0100

    Add files via upload

--- patient2.php
+++ patient2.php
@@ -10,36 +10,55 @@
 
 include("connexion_bd.php");
 
-$_SESSION['numerodoss'] = htmlspecialchars($_POST['numdoss']); 
-
-echo "Quelle visite du patient ".$_SESSION['numerodoss']." souhaitez-vous consulter ? <br /> \n";
+echo "Le patient sélectionné est le patient numéro ".$_SESSION['numerodoss'].". </br>";
 $requete = $bdd ->prepare('select Date from tab_suivi where N_dossier= :p_numdossier');
 $requete -> execute(array(':p_numdossier' => $_SESSION['numerodoss']));
-	
 ?>
+</br>
+<form method="POST" action="ATCD.php">
+	<input type="submit" value="Voir les antécédents du patient">
+</form>
+</br>
+<form method="POST" action="prescriptions.php">
+	<input type="submit" value="Voir les préscriptions du patient">
+</form>
+</br>
+<form method="POST" action="nouveau_suivi.php">
+	<input type="submit" value="Ajouter une nouvelle fiche de suivi à ce patient">
+</form>
+</br>
+<form method="POST" action="suprimer_patient.php">
+	<input type="submit" value="supprimer ce patient">
+</form>
+</br>
 
-<form method="POST" action="patient3.php">
+<form method="POST" action="action_visite.php">
     <select name="jourvis">
         <option selected="selected">Date de la visite</option>
         <?php
         $jour = $requete->fetch();
         while ($jour)
-        {	
-        	foreach($jour as $item)
-        	{
-            		echo "<option value=".$jour['Date'].">".$jour['Date']."</option>";
-            		$jour = $requete->fetch();
+        	{	
+        		foreach($jour as $item)
+        			{
+            			echo "<option value=".$jour['Date'].">".$jour['Date']."</option>";
+            			$jour = $requete->fetch();
+        			}
         	}
-        }
-
+			
         ?>
-        
-     <!-- A completer par Antoine -->   
-	<option selected="selected">Date de la visite</option>"Visite initiale"
+	<option selected="selected">Visite initiale</option>
     </select>
-    <input type="submit" value="Submit">
-</form>
 	
+	<input type="submit" value="Ok">
+	
+</form>
+
+
+	
+<?php
+
+?>
 
 
 </body>
