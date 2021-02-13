e47ce8131b29baf24f3ca696b6009d61c6be1239
gkmzinsou <32025045+gkmzinsou@users.noreply.github.com>
2021-02-13 21:46:04 +0100

    Update mes_patients.php

--- mes_patients.php
+++ mes_patients.php
@@ -5,10 +5,10 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<title> MES PATIENTS </title>
-	</head>
+<head>
+<meta charset="utf-8" />
+	<title> MES PATIENTS </title>
+</head>
 <body>
 
 
@@ -22,33 +22,38 @@ include("connexion_bd.php");
 $req = $bdd -> prepare('SELECT tab_patient.* FROM tab_suivi 
 INNER JOIN tab_patient ON tab_suivi.N_dossier = tab_patient.Num_dossier 
 INNER JOIN tab_utilisateurs ON tab_suivi.ID_med= tab_utilisateurs.ID_prof
-WHERE tab_utilisateurs.ID_prof = :ID_utilisateur');
+WHERE tab_utilisateurs.ID_prof = :ID_utilisateur
+ORDER BY tab_patient.Num_dossier, tab_patient.Nom, tab_patient.Pren');
 $req->execute(array(':ID_utilisateur' => $_SESSION['ID_utilisateur']));
 
 ?>
 
-<form method="POST" action="mes_choix.php">
+<form method="POST" action="patient2.php">
     <select name="numdoss">
         <option selected="selected">Sélectionnez votre patient</option>
         <?php
         $ligne = $req->fetch();
         while ($ligne)
-        	{	
-        		foreach($ligne as $item)
-        			{
-            			echo "<option value=".$ligne['Num_dossier'].">".$ligne['Nom']." ".$ligne['Pren']." ".$ligne['Date_nais']."</option>";
-            			$ligne = $req->fetch();
-        			}
+        {	
+        	foreach($ligne as $item)
+        	{
+            	echo "<option value=".$ligne['Num_dossier'].">".$ligne['Num_dossier']." ".$ligne['Nom']." ".$ligne['Pren']."</option>";
+            	$ligne = $req->fetch();
         	}
+        }
         $req->closeCursor() ;
         
         ?>
     </select>
     <table>
 	<tr><td> <input type = "submit" name = "BTN_PAT" value = "VALIDER"></td></tr>
-    <tr><td> <input type = "submit" name = "BTN_AJT" value = "AJOUT D'UN NOUVEU PATIENT"></td></tr>
 	</table> 
-    
+    </form>
+
+<form method="POST" action="#ajout">
+    <table>
+	<tr><td> <input type = "submit" name = "BTN_MOD" value = "AJOUT D'UN PATIENT"></td></tr>
+	</table> 
 </form>
 
 <br/> <a href="accueil.html"> Page d'accueil </a>
