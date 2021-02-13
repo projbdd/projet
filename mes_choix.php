96577fc70bd7049dd3ef6377ae4c0f63813c01f5
gkmzinsou <32025045+gkmzinsou@users.noreply.github.com>
2021-02-13 17:12:04 +0100

    Update mes_choix.php

--- mes_choix.php
+++ mes_choix.php
@@ -13,11 +13,11 @@ include("connexion_bd.php");
 
 //le bouton Valider a été activé
 if (isset($_POST['BTN_PAT'])) 
-	{
+	{	
 		echo "Vous avez selectionner le patient : "." ".$_POST['numdoss']."<br />";
 		echo "<a href='patient2.php'> Accéder aux informations du patient </a>";
 		$_SESSION['numerodoss'] = htmlspecialchars($_POST['numdoss']); 
-		
+		echo "<br/> <a href='accueil.html'> Page d'accueil </a>";
 	} 
 // le bouton Ajout d'un patient est activé
 else if (isset($_POST['BTN_AJT']))  
@@ -30,3 +30,4 @@ else if (isset($_POST['BTN_AJT']))
 
 </body>
 </html>
+
