9b0b91ab0e0348a69d85c2eb6f73683a1d264579
gkmzinsou <32025045+gkmzinsou@users.noreply.github.com>
2021-02-14 00:13:30 +0100

    Update mes_infos.php

--- mes_infos.php
+++ mes_infos.php
@@ -2,39 +2,61 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 		<meta charset="utf-8" />
 		<title> MES INFORMATIONS  </title>
-	</head>
+	<link rel="stylesheet" media="screen" href="feuille_style.css">
+</head>
 <body>
+
+   <div class="navbar">
+        <a href="motpasse_corrigé.html">Déconnexion</a>
+        <div class="dropdown">
+        <button class="dropbtn">Mon compte
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="mes_infos.php">Mes informations</a> 
+          <a href="mes_collègues.php">Mes collègues</a>
+        </div>
+      </div>
+    </div>
 
 
 <?php
-
 include("connexion_bd.php");
 
-
-$req = $bdd -> prepare('SELECT * FROM tab_utilisateurs WHERE ID_prof = :ID_utilisateur');
+$req = $bdd -> prepare('SELECT * FROM tab_utilisateurs WHERE ID_prof = :ID_utilisateur AND NOT ID_prof = 999');
 $req->execute(array(':ID_utilisateur' => $_SESSION['ID_utilisateur']));
 
 
-$ligne = $req->fetch();
+//Cherche index à partir de la 2e colonne, donc le 2e élement du array
+$ligne = $req->fetch(); 
 
+//Cherche index à partir du premier élement du array
+$label = array("Nom", "Prénom", "Age", "Téléphone"); 
 
-for($i=1;$i<5;$i++)
-{	
-	echo "<td>".$ligne[$i]."<br /> </td>";
+if ($ligne)
+{
+	echo "<table>";
+	for ($i=0;$i<=3;$i++)
+	{
+		echo "<tr><td><strong>".$label[$i]." : </strong> </td>  <td>".$ligne[$i+1]."</td></tr>";
+
+	}
+	echo "</table>";
 }
 
 $req->closeCursor() ;
-
-
-
 ?>
 
-<br/> <a href="mes_patients.php"> Mes patients</a>
-<br/> <a href="mes_collègues.php"> Mes collègues </a>
-<br/> <a href="accueil.html"> Page d'accueil </a>
+
+<table align="center">
+<tr><br/> <a href="mes_patients.php"> Mes patients</a></tr>
+<tr><br/> <a href="mes_collègues.php"> Mes collègues </a></tr>
+<tr><br/> <a href="accueil.html"> Page d'accueil </a></tr>
+</table>
+
 
 
 <!--
