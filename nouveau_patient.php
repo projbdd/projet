b5c74f2ba91dd80105789bb9c07786e22a6d120a
LyviaMg <78866632+LyviaMg@users.noreply.github.com>
2021-02-14 17:35:07 +0100

    Update nouveau_patient.php

--- nouveau_patient.php
+++ nouveau_patient.php
@@ -5,6 +5,23 @@
 		<title>Ajouter un nouveau patient </title>
 </head>
 <body>
+	
+<div class="navbar">
+    <a href="motpasse_corrigé.html">Déconnexion</a>
+    <div class="dropdown">
+        <button class="dropbtn">Mon compte
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="mes_infos.php">Mes informations</a>
+          <a href="mes_collègues.php">Mes collègues</a>
+        </div>
+    </div>
+</div>
+    
+<!-- Reste de la page-->
+<div class = "main">
+	
 <?php
 include("connexion_bd.php");
 ?>
@@ -77,6 +94,6 @@ include("connexion_bd.php");
 	</fieldset>
 </form>
 
-
+</div>
 </body>
 </html>
