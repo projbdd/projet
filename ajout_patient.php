2709e8ee8ef477444ac4d25cb36f9044cb74c2b5
AntoineGlb <78866642+AntoineGlb@users.noreply.github.com>
2021-02-15 17:14:24 +0100

    Update ajout_patient.php

--- ajout_patient.php
+++ ajout_patient.php
@@ -29,7 +29,7 @@
 include("connexion_bd.php");
 
 
-$requete = $bdd -> prepare('insert into tab_patients values(:p_num_dossier, :p_date_debut_suivi, :p_nom, :p_pren, :p_adressage, :p_typ_consul, :p_sexe, :p_date_nais, :p_csp, :p_ethnie, :p_com_ou_pays_nais, :p_anne_dec_kc, :p_lat_man, :p_tabagisme_actif, :p_nb_cig_jr, :p_nb_annees, :p_tabagisme_passif, :p_situ_od, :p_situ_og)');
+$requete = $bdd -> prepare('insert into tab_patient values(:p_num_dossier, :p_date_debut_suivi, :p_nom, :p_pren, :p_adressage, :p_typ_consul, :p_sexe, :p_date_nais, :p_csp, :p_ethnie, :p_com_ou_pays_nais, :p_anne_dec_kc, :p_lat_man, :p_tabagisme_actif, :p_nb_cig_jr, :p_nb_annees, :p_tabagisme_passif, :p_situ_od, :p_situ_og)');
 
 $requete -> execute(array(':p_num_dossier' => $_POST['numdo'], 
 							':p_date_debut_suivi' => $_POST['datedeb'], 
