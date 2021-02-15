7140c815fc24f509ea716f285332088eaea6eceb
gkmzinsou <32025045+gkmzinsou@users.noreply.github.com>
2021-02-15 20:12:56 +0100

    Update page_stats.php

--- page_stats.php
+++ page_stats.php
@@ -7,18 +7,11 @@
 
 <body>
 
-<div class="navbar">
-    <a href="motpasse_corrigé.html">Déconnexion</a>
-    <div class="dropdown">
-        <button class="dropbtn">Mon compte
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="mes_infos.php">Mes informations</a>
-          <a href="mes_collègues.php">Mes collègues</a>
-        </div>
-    </div>
-</div>
+<?php
+//Barre de navigation 
+include("barr_navig.html");
+?>
+ 
  
 <!-- Reste de la page-->
 <div class = "main">
