964a46a647ee976f82f1063763b4e84239d695ab
klervii <78863584+klervii@users.noreply.github.com>
2021-02-15 23:46:19 +0100

    Add files via upload

---
+++ ajout_visite.php
@@ -0,0 +1,65 @@
+<?php session_start(); ?>
+<html>
+	<head>
+		<meta charset="utf-8" />	
+		<title>Ajouter un nouveau patient </title>
+		<link rel="stylesheet" media="screen" href="feuille_style.css">
+</head>
+<body>
+	
+<?php
+//Barre de navigation 
+include("barr_navig.html");
+?>
+
+    
+<!-- Reste de la page-->
+<div class = "main">
+	
+<?php
+// connexion  à la base de données
+include("connexion_bd.php");
+        $suivi = $bdd->prepare("SELECT max(ID_suivi)+1 as max FROM tab_suivi");
+        $suivi ->execute();;
+        $rep = $suivi->fetch();
+		$_max= $rep['max'];
+$numdoss=$_SESSION['numerodoss'];
+
+
+$requete1 = $bdd->prepare('select avg(nbsuivi) as moyenne from (select p.Num_dossier, count(s.ID_suivi) as nbsuivi from tab_suivi s inner join tab_patient p on s.N_dossier= p.Num_dossier where p.Sexe=1 group by p.Num_dossier ) AS T');
+$requete1->execute();
+$ligne1 = $requete1->fetch();
+
+$requete = $bdd -> prepare('insert into tab_suivi values(:p_id_suivi, :p_type_consult, :p_numdoss, :p_date, :p_sign_fonc, :p_BAV_rap, :p_BAV_lente, :p_Halo, :p_Photophobie, :p_ddblee, :p_rougeurs, :p_autre, :p_autre_det, :p_frott, :p_lentilles, :p_adapt, :p_tolerance, :p_nbhl, :p_nbjl, :p_idmed )');
+
+$requete -> execute(array(':p_id_suivi' => $_max, 
+							':p_type_consult' => $_POST['typeconsult'],
+							':p_numdoss'=> $numdoss,
+  						    ':p_date' => $_POST['date'],
+							':p_sign_fonc' => $_POST['sign_fonc'], 
+							':p_BAV_rap' => $_POST['BAVrap'], 
+							':p_BAV_lente' => $_POST['BAVlente'], 
+							':p_Halo' => $_POST['Halo'], 
+							':p_Photophobie' => $_POST['Photophobie'], 
+							':p_ddblee' => $_POST['ddblee'], 
+							':p_rougeurs' => $_POST['BAVlente'], 
+							':p_autre' => $_POST['autres'], 
+							':p_autre_det' => $_POST['autres_det'], 
+							':p_frott' => $_POST['frott'], 
+							':p_lentilles' => $_POST['lentilles'], 
+							':p_adapt' => $_POST['adapt'], 
+							':p_tolerance' => $_POST['tolerance'], 
+							':p_nbhl' => $_POST['nbhl'], 
+							':p_nbjl' => $_POST['nbjl'], 
+							':p_idmed' => $_POST['idmed'] ) ); 
+
+
+if ($requete != null){
+	echo "</br> La visite numéro " .$_max." du ".$_POST['date']." du patient numéro ".$numdoss." à été ajoutée avec succès ! </br>";
+}
+?>
+
+
+</div>
+</body>
+</html>
---
+++ nouveau_suivi.php
@@ -0,0 +1,105 @@
+<?php session_start(); ?>
+<html>
+	<head>
+		<meta charset="utf-8" />	
+		<title>Nouveau suivi</title>
+		<link rel="stylesheet" media="screen" href="feuille_style.css">
+</head>
+<body>
+
+<div class="navbar">
+    <a href="motpasse_corrigé.html">Déconnexion</a>
+    <div class="dropdown">
+        <button class="dropbtn">Mon compte
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="mes_infos.php">Mes informations</a>
+          <a href="mes_collègues.php">Mes collègues</a>
+        </div>
+    </div>
+</div>
+
+    
+<!-- Reste de la page-->
+<div class = "main">
+
+<?php 
+
+include("connexion_bd.php");
+	
+?>
+ 
+<form method="POST" action="ajout_visite.php">
+	<fieldset><legend><h3>Ajout d'une nouvelle visite</h3></legend>
+		Type de consultation : <select name="typeconsult">
+			<option value="0">Premier diagnostic</option>
+			<option value="1">Premier avis</option>
+			<option value="2">Second ou énième avis</option>
+		</select></br>
+		Date  : <input name='date' type="datetime-local"></br>
+		Signes fonctionnels et détails : <input name='sign_fonc'></br>
+		BAV Rapide : <select name="BAVrap">
+			<option value="0"> Non </option>
+			<option value="-1"> Oui </option>
+		</select></br>
+		BAV Lente : <select name="BAVlente">
+			<option value="0"> Non </option>
+			<option value="-1"> Oui </option>
+		</select></br>
+		Halo nocturnes : <select name="Halo">
+			<option value="0"> Non </option>
+			<option value="-1"> Oui </option>
+		</select></br>		
+		Photophobie : <select name="Photophobie">
+			<option value="0"> Non </option>
+			<option value="-1"> Oui </option>
+		</select></br>		
+		Vision DDBLEE : <select name="ddblee">
+			<option value="0"> Non </option>
+			<option value="-1"> Oui </option>
+		</select></br>		
+		Rougeurs oculaires : <select name="rougeurs">
+			<option value="0"> Non </option>
+			<option value="-1"> Oui </option>
+		</select></br>
+		Autres : <select name="autres">
+			<option value="0"> Non </option>
+			<option value="-1"> Oui </option>
+		</select></br>
+		Autres symptomes : <input name='autres_det'></br>
+		Frottement occulaires : <select name="frott">
+			<option value="0">Pas du tout</option>
+			<option value="1">Un peu</option>
+			<option value="2">Modérement </option>
+			<option value="3">Beaucoup</option>
+			<option value="4">Constament</option>
+		</select></br>
+		Port de lentilles : <select name="lentilles">
+			<option value="0"> Oui </option>
+			<option value="1"> Non </option>
+		</select></br>
+		Adaptation aux lentilles : <select name="adapt">
+			<option value="1">Première adaptation </option>
+			<option value="2"> Contrôle lentille adaptée CRNK </option>
+			<option value="3">Contrôle lentille non adaptée CRNK</option>
+		</select></br>
+		Tolérance: <select name="tolerance">
+			<option value="0"> Oui </option>
+			<option value="1"> Non </option>
+		</select></br>
+		
+		Nombre d'heure pendant lesquelles les lentilles sont portées par jour : <input name="nbhl" type="number"></br>
+		Nombre de jours pendant lesquels les lentilles sont portées par semaine : <input name="nbjl" type="number"></br>
+		Confirmez votre numéro d'identification médecin : <input name="idmed" type="number"></br>
+
+		
+		<input type = "submit" value = "Ajouter cette visite"/></br>
+	</fieldset>
+</form>
+
+
+</div>
+
+</body>
+</html>
