e9d4457e378dd5b7efd3a36e5485496955869656
LyviaMg <78866632+LyviaMg@users.noreply.github.com>
2021-02-17 19:35:43 +0100

    Add files via upload

--- Ajout_user.php
+++ Ajout_user.php
@@ -40,58 +40,59 @@
         // Requête pour ajout
         $req = $bdd -> prepare('insert into tab_utilisateurs values (:p_prof, :p_nom, :p_prenom, :p_age, :p_tel, :p_user, :p_mdp, :p_type)');
 
-        // Exécution de la requête
-        $req -> execute(array(':p_prof' => $max[0], ':p_nom' => htmlspecialchars($_POST['nomMed']), ':p_prenom' => htmlspecialchars($_POST['prenomMed']), ':p_age' => htmlspecialchars($_POST['ageMed']), ':p_tel' => htmlspecialchars($_POST['telMed']), ':p_user' => htmlspecialchars($_POST['ID']), ':p_mdp' => htmlspecialchars($_POST['pwd']), 'p_type' => $_POST['typeMed']));
+        // Exécution de la requête : il faut que toutes les valeurs soient renseignée        if (htmlspecialchars($_POST['nomMed']) != NULL) {
+            if (htmlspecialchars($_POST['prenomMed']) != NULL) {
+                if (htmlspecialchars($_POST['telMed']) != NULL) {
+                    if (htmlspecialchars($_POST['ageMed']) != NULL) {
+                        if (htmlspecialchars($_POST['typeMed']) != 'Choissisez une fonction') {
+                            if (htmlspecialchars($_POST['ID']) != NULL) {
+                                if ($check[0] == NULL) {
+                                    if (htmlspecialchars($_POST['pwd']) != NULL) {
+                                        if (htmlspecialchars($_POST['pwd']) == htmlspecialchars($_POST['conf_pwd'])) { // Contrôle sur les mots de passe
 
-        if ($req != null) { 
-            if (htmlspecialchars($_POST['nomMed']) != NULL) {
-                if (htmlspecialchars($_POST['prenomMed']) != NULL) {
-                    if (htmlspecialchars($_POST['telMed']) != NULL) {
-                        if (htmlspecialchars($_POST['ageMed']) != NULL) {
-                            if (htmlspecialchars($_POST['typeMed']) != 'Choissisez une fonction') {
-                                if (htmlspecialchars($_POST['ID']) != NULL) {
-                                    if ($check[0] == NULL) {
-                                        if (htmlspecialchars($_POST['pwd']) != NULL) {
-                                            if (htmlspecialchars($_POST['pwd']) == htmlspecialchars($_POST['conf_pwd'])) { // Contrôle sur les mots de passe
-                                                echo "</br> Nouvel utilisateur ".htmlspecialchars($_POST['prenomMed'])." ".htmlspecialchars($_POST['nomMed'])." rajouté avec succès. </br></br>";
-                                                echo "<a href='EspaceDataM.php'>Retour</a> ";
+                                                $req -> execute(array(':p_prof' => $max[0], ':p_nom' => htmlspecialchars($_POST['nomMed']), ':p_prenom' => htmlspecialchars($_POST['prenomMed']), ':p_age' => htmlspecialchars($_POST['ageMed']), ':p_tel' => htmlspecialchars($_POST['telMed']), ':p_user' => htmlspecialchars($_POST['ID']), ':p_mdp' => htmlspecialchars($_POST['pwd']), 'p_type' => $_POST['typeMed']));
 
-                                            } else {
+                                                if ($req != null) { 
+
+                                                    echo "</br> Nouvel utilisateur ".htmlspecialchars($_POST['prenomMed'])." ".htmlspecialchars($_POST['nomMed'])." rajouté avec succès. </br></br>";
+                                                    echo "<a href='EspaceDataM.php'>Retour</a> ";
+                                                }
+
+                                        } else {
                                                     echo "</br> Les deux mots de passe doivent être identiques.</br></br>";
                                                     echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
-                                            } 
-                                        } else {
+                                        } 
+                                    } else {
                                             echo "</br>Veuillez définir un mot de passe pour l'utilisateur l'utilisateur.</br></br>";
                                             echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
-                                        }
-                                    } else {
-                                        echo "</br>Cet identifiant est déja utilisé pour un autre utilisateur.</br></br>";
-                                        echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
                                     }
                                 } else {
-                                    echo "</br>Veuillez définir un identifiant pour l'utilisateur.</br></br>";
-                                    echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
+                                        echo "</br>Cet identifiant est déja utilisé pour un autre utilisateur.</br></br>";
+                                        echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
                                 }
                             } else {
-                                echo "</br>Veuillez renseigner la fonction de l'utilisateur.</br></br>";
-                                echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
+                                    echo "</br>Veuillez définir un identifiant pour l'utilisateur.</br></br>";
+                                    echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
                             }
                         } else {
-                            echo "</br>Veuillez renseigner l'âge de l'utilisateur.</br></br>";
-                            echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
+                                echo "</br>Veuillez renseigner la fonction de l'utilisateur.</br></br>";
+                                echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
                         }
                     } else {
-                        echo "</br>Veuillez renseigner le numéro de téléphone de l'utilisateur.</br></br>";
-                        echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
+                            echo "</br>Veuillez renseigner l'âge de l'utilisateur.</br></br>";
+                            echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
                     }
                 } else {
-                    echo "</br>Veuillez renseigner le prénom de l'utilisateur.</br></br>";
-                    echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
+                        echo "</br>Veuillez renseigner le numéro de téléphone de l'utilisateur.</br></br>";
+                        echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
                 }
             } else {
+                    echo "</br>Veuillez renseigner le prénom de l'utilisateur.</br></br>";
+                    echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
+            }
+        } else {
                 echo "</br>Veuillez renseigner le nom de l'utilisateur.</br></br>";
                 echo '<a href = "EspaceDataM.php"/>Réessayer</a>';
-            }
         }
 
         $req->closeCursor();
--- EspaceDataM.php
+++ EspaceDataM.php
@@ -1,5 +1,7 @@
 <?php session_start()?>
 
+<!------ LYVIA MAGLOIRE ------->
+
 <html>
 <head>
 <meta charset="utf-8" />
