8f04a742dbabfb6d9d5062f2f32298cf891b3944
AntoineGlb <78866642+AntoineGlb@users.noreply.github.com>
2021-02-17 10:20:20 +0100

    Update patient2.php

--- patient2.php
+++ patient2.php
@@ -1,3 +1,5 @@
+<!-- Page rédigée par Antoine -->
+
 <?php session_start(); ?>
 
 <html>
@@ -19,6 +21,7 @@ include("barr_navig.html");
 
 <?php 
 
+// connexion à la base de données
 include("connexion_bd.php");
 
 // Stocker le numéro de dossier dans une variable session seulement si sélectionné à partir du formulaire mes_patients.php
@@ -39,12 +42,17 @@ if ($_SESSION['numerodoss'] == "Sélectionnez votre patient")
 
 // Ne rien faire si $_SESSION['numerodoss'] est déja renseignée 
 
-
+//affichage du patient
 echo "<br/> Le patient sélectionné est le patient numéro <strong>".$_SESSION['numerodoss']."</strong>. </br>";
+	
+// requête pour trouver toutes ses visites
 $requete = $bdd ->prepare('select Date from tab_suivi where N_dossier= :p_numdossier');
+// execution de la requete
 $requete -> execute(array(':p_numdossier' => $_SESSION['numerodoss']));
 ?>
 </br>
+	
+<!-- boutons pour les différentes actions possibles -->
 <form method="POST" action="ATCD.php">
 	<input id = "bouton" type="submit" value="Voir les antécédents du patient">
 </form>
@@ -65,7 +73,7 @@ $requete -> execute(array(':p_numdossier' => $_SESSION['numerodoss']));
 <?php 
 echo "Selectionnez une visite sur laquelle vous voulez des informations, ou sur laquelle vous voulez faire des modifications : </br>";
 ?>
-
+<!-- selection de la visite que l'on veut voir -->
 <form method="POST" action="action_visite.php">
     <select id = "selectbox" name="jourvis">
         <option selected="selected">Date de la visite</option>
@@ -81,6 +89,7 @@ echo "Selectionnez une visite sur laquelle vous voulez des informations, ou sur 
         	}
 			
         ?>
+	<!-- possibilité de voir la visite initiale -->
 	<option selected="selected">Visite initiale</option>
     </select>
 	
