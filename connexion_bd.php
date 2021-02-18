290772f1ea5421151311105aad6ef320c92f1840
gkmzinsou <32025045+gkmzinsou@users.noreply.github.com>
2021-02-18 11:02:18 +0100

    Update connexion_bd.php

--- connexion_bd.php
+++ connexion_bd.php
@@ -1,7 +1,14 @@
-
 <?php
 // se connecter à la base de données
 // en testant la présence d'erreur
-$bdd = new PDO('mysql:host=localhost;dbname=projet', 'aurélien','aurélien');
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=projet', 'biostat', 'biostat', 	array(PDO::ATTR_ERRMODE => 	PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+};
 
 ?>
+
