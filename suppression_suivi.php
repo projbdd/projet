42f2fc57dcde78acab91b82bcf7387d079c21dd8
klervii <78863584+klervii@users.noreply.github.com>
2021-02-18 11:07:53 +0100

    Update suppression_suivi.php

--- suppression_suivi.php
+++ suppression_suivi.php
@@ -22,8 +22,8 @@ include("connexion_bd.php");
 
 
 //$_SESSION['numerodoss']=$_POST['numdoss'];
-	$requete1 = $bdd->prepare('delete from tab_suivi where N_dossier= :p_numdossier AND Date= :p_datecons');
-	$requete1->execute(array(':p_numdossier' => $_SESSION['numerodoss'], ':p_datecons' => $_SESSION['jourvis']));
+	$requete1 = $bdd->prepare('delete from tab_suivi where N_dossier= :p_numdossier AND Date= :p_datecons');//préparation de la commande de suppression ;
+	$requete1->execute(array(':p_numdossier' => $_SESSION['numerodoss'], ':p_datecons' => $_SESSION['jourvis'])); // récupération de la bonne ligne;
 	
 	echo"<h2> Visite à la date ".$_SESSION['jourvis']." du patient ".$_SESSION['numerodoss']." supprimée","</h2></br>";
 	
