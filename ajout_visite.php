cb4955a5f5aafb5b94e341010f83903a4ce994ed
klervii <78863584+klervii@users.noreply.github.com>
2021-02-18 19:24:54 +0100

    Update ajout_visite.php

--- ajout_visite.php
+++ ajout_visite.php
@@ -1,5 +1,7 @@
 <?php session_start(); ?>
 <html>
+<!------ Klervi LE GALL ------->
+	
 	<head>
 		<meta charset="utf-8" />	
 		<title>Ajouter une nouvelle visite </title>
@@ -20,20 +22,20 @@ include("barr_navig.html");
 // connexion  à la base de données
 include("connexion_bd.php");
 
-$suivi = $bdd->prepare("SELECT max(ID_suivi)+1 as max FROM tab_suivi");
+$suivi = $bdd->prepare("SELECT max(ID_suivi)+1 as max FROM tab_suivi"); // Récupération de l'id de visite à attribuer 
 $suivi ->execute();;
 $rep = $suivi->fetch();
 $_max= $rep['max'];
 
-$numdoss=$_SESSION['numerodoss'];
+$numdoss=$_SESSION['numerodoss']; // récupération de l'id du patient
 
 
-$requete = $bdd -> prepare('insert into tab_suivi values(:p_id_suivi, :p_type_consult, :p_numdoss, :p_date, :p_sign_fonc, :p_BAV_rap, :p_BAV_lente, :p_Halo, :p_Photophobie, :p_ddblee, :p_rougeurs, :p_autre, :p_autre_det, :p_frott, :p_lentilles, :p_adapt, :p_tolerance, :p_nbhl, :p_nbjl, :p_idmed )');
+$requete = $bdd -> prepare('insert into tab_suivi values(:p_id_suivi, :p_type_consult, :p_numdoss, :p_date, :p_sign_fonc, :p_BAV_rap, :p_BAV_lente, :p_Halo, :p_Photophobie, :p_ddblee, :p_rougeurs, :p_autre, :p_autre_det, :p_frott, :p_lentilles, :p_adapt, :p_tolerance, :p_nbhl, :p_nbjl, :p_idmed )'); // préparation de l'insertion
 
 $requete -> execute(array(':p_id_suivi' => $_max, 
 							':p_type_consult' => $_POST['typeconsult'],
 							':p_numdoss'=> $numdoss,
-  						    ':p_date' => $_POST['date'],
+  						        ':p_date' => $_POST['date'],
 							':p_sign_fonc' => $_POST['sign_fonc'], 
 							':p_BAV_rap' => $_POST['BAVrap'], 
 							':p_BAV_lente' => $_POST['BAVlente'], 
@@ -49,11 +51,11 @@ $requete -> execute(array(':p_id_suivi' => $_max,
 							':p_tolerance' => $_POST['tolerance'], 
 							':p_nbhl' => $_POST['nbhl'], 
 							':p_nbjl' => $_POST['nbjl'], 
-							':p_idmed' => $_SESSION['ID_utilisateur'])); 
+							':p_idmed' => $_SESSION['ID_utilisateur'])); //execution de la requête en récupérant les résultats du questionnaire
 
 
 if ($requete != null){
-	echo "</br><h3> La visite numéro " .$_max." du ".$_POST['date']." du patient numéro ".$numdoss." à été ajoutée avec succès ! </h3></br>";
+	echo "</br><h3> La visite numéro " .$_max." du ".$_POST['date']." du patient numéro ".$numdoss." à été ajoutée avec succès ! </h3></br>"; // vérification 
 }
 ?>
 
