d0e2394aa5cfb80ea91a44a72782adcd7499865c
gkmzinsou <32025045+gkmzinsou@users.noreply.github.com>
2021-02-18 12:31:18 +0100

    Update nouveau_suivi.php

--- nouveau_suivi.php
+++ nouveau_suivi.php
@@ -7,19 +7,11 @@
 </head>
 <body>
 
-<div class="navbar">
-    <a href="motpasse_corrigé.html">Déconnexion</a>
-    <div class="dropdown">
-        <button class="dropbtn">Mon compte
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="mes_infos.php">Mes informations</a>
-          <a href="mes_collègues.php">Mes collègues</a>
-        </div>
-    </div>
-</div>
-
+<?php
+//Barre de navigation 
+include("barr_navig.html");
+?>
+    
     
 <!-- Reste de la page-->
 <div class = "main">
@@ -31,70 +23,91 @@ include("connexion_bd.php");
 ?>
  
 <form method="POST" action="ajout_visite.php">
-	<fieldset><legend><h3>Ajout d'une nouvelle visite</h3></legend>
-		Type de consultation : <select name="typeconsult" required >
+
+	<fieldset id = "connec" ><legend><h3> Ajout d'une nouvelle visite </h3></legend>
+	
+		<?php echo " Numéro de dossier : ".$_SESSION['numerodoss']."</br></br>"; ?>
+		
+		<table id = connec1>	
+			
+		<tr><td><b> Type de consultation : </b></td><td><select name="typeconsult" >
 			<option value="1">Suivi</option>
 			<option value="2">Contactologie</option>
 			<option value="3">Chirurgie</option>
-		</select></br>
-		Date  : <input name='date' type="datetime-local" value ="2021-01-01T00:00" required></br>
-		Signes fonctionnels et détails : <input name='sign_fonc'></br>
-		BAV Rapide : <select name="BAVrap" >
+		</select></td></tr>
+		
+		<tr><td><b> Date  :  </b></td><td><input name='date' type="datetime-local" value ="2021-01-01T00:00"></td></tr>
+		
+		<tr><td><b>Signes fonctionnels et détails : </b></td><td><input name='sign_fonc' required></td></tr>
+		
+		<tr><td><b>BAV Rapide : </b></td><td><select name="BAVrap" >
 			<option value="0"> Non </option>
 			<option value="-1"> Oui </option>
-		</select></br>
-		BAV Lente : <select name="BAVlente">
+		</select></td></tr>
+		
+		<tr><td><b>BAV Lente : </b></td><td><select name="BAVlente">
 			<option value="0"> Non </option>
 			<option value="-1"> Oui </option>
-		</select></br>
-		Halo nocturnes : <select name="Halo">
+		</select></td></tr>
+		
+		
+		<tr><td><b>Halo nocturnes : </b></td><td><select name="Halo">
 			<option value="0"> Non </option>
 			<option value="-1"> Oui </option>
-		</select></br>		
-		Photophobie : <select name="Photophobie">
+		</select></td></tr>
+			
+		<tr><td><b>Photophobie : </b></td><td><select name="Photophobie">
 			<option value="0"> Non </option>
 			<option value="-1"> Oui </option>
-		</select></br>		
-		Vision dédoublée : <select name="ddblee">
+		</select></td></tr>	
+		
+		<tr><td><b>Vision dédoublée : </b></td><td><select name="ddblee">
 			<option value="0"> Non </option>
 			<option value="-1"> Oui </option>
-		</select></br>		
-		Rougeurs oculaires : <select name="rougeurs">
+		</select></td></tr>	
+		
+		<tr><td><b>Rougeurs oculaires : </b></td><td><select name="rougeurs">
 			<option value="0"> Non </option>
 			<option value="-1"> Oui </option>
-		</select></br>
-		Autres : <select name="autres">
+		</select></td></tr>
+		
+		<tr><td><b>Autres : </b></td><td><select name="autres">
 			<option value="0"> Non </option>
 			<option value="-1"> Oui </option>
-		</select></br>
-		Autres symptomes : <input name='autres_det'></br>
-		Frottement occulaires : <select name="frott" required>
+		</select></td></tr>
+		
+		<tr><td><b>Autres symptomes : </b></td><td><input name='autres_det'required></td></tr>
+		
+		<tr><td><b>Frottement occulaires : </b></td><td><select name="frott" >
 			<option value="0">Pas du tout</option>
 			<option value="1">Un peu</option>
 			<option value="2">Modérement </option>
 			<option value="3">Beaucoup</option>
 			<option value="4">Constament</option>
-		</select></br>
-		Port de lentilles : <select name="lentilles" required>
+		</select></td></tr>
+		
+		<tr><td><b>Port de lentilles : </b></td><td><select name="lentilles" >
 			<option value="1"> Oui </option>
 			<option value="0"> Non </option>
-		</select></br>
-		Adaptation aux lentilles : <select name="adapt" >
+		</select></td></tr>
+		
+		<tr><td><b>Adaptation aux lentilles : </b></td><td><select name="adapt" >
 			<option value="1">Première adaptation </option>
 			<option value="2"> Contrôle lentille adaptée CRNK </option>
 			<option value="3">Contrôle lentille non adaptée CRNK</option>
-		</select></br>
-		Tolérance: <select name="tolerance">
+		</select></td></tr>
+		
+		<tr><td><b>Tolérance: </b></td><td><select name="tolerance">
 			<option value="0"> Oui </option>
 			<option value="1"> Non </option>
-		</select></br>
+		</select></td></tr>
 		
-		Nombre d'heure pendant lesquelles les lentilles sont portées par jour : <input name="nbhl" type="number"></br>
-		Nombre de jours pendant lesquels les lentilles sont portées par semaine : <input name="nbjl" type="number"></br>
-		Confirmez votre numéro d'identification médecin : <input name="idmed" type="number"></br>
-
+		<tr><td><b>Nombre d'heure pendant lesquelles  </br> les lentilles sont portées par jour : </b></td><td><input name="nbhl" type="number" min="0" max="24" required></td></tr>
+		<tr><td><b>Nombre de jours pendant lesquels  </br> les lentilles sont portées par semaine : </b></td><td><input name="nbjl" type="number"min="0" max="7" required></td></tr>
 		
-		<input type = "submit" value = "Ajouter cette visite"/></br>
+		</table>
+		
+		<br/><br/></br><input type = "submit" value = "Ajouter cette visite"/></br>
 	</fieldset>
 </form>
 
